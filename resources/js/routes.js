4b8f03b5d4cfccb38e2fcd44621ec66a7f42019b
MostafaRhazlani <mo.rhazlani@gmail.com>
2023-12-19 17:19:44 +0100

    create pages

---
+++ resources/js/routes.js
@@ -0,0 +1,37 @@
+import Dashboard from './components/Dashboard.vue';
+import ListAppointments from './pages/appointments/ListAppointments.vue';
+import ListUsers from './pages/users/ListUsers.vue';
+import UpdateSettings from './pages/settings/UpdateSetting.vue';
+import UpdateProfile from './pages/profile/UpdateProfile.vue';
+
+export default [
+    {
+        path: '/admin/dashboard',
+        name: 'admin.dashboard',
+        component: Dashboard,
+    },
+
+    {
+        path: '/admin/appointments',
+        name: 'admin.appointments',
+        component: ListAppointments,
+    },
+
+    {
+        path: '/admin/users',
+        name: 'admin.users',
+        component: ListUsers,
+    },
+
+    {
+        path: '/admin/settings',
+        name: 'admin.settings',
+        component: UpdateSettings,
+    },
+
+    {
+        path: '/admin/profile',
+        name: 'admin.profile',
+        component: UpdateProfile,
+    },
+]
