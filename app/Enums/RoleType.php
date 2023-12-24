bcb1902113d201e898f9b2248b20b36ffacb38c8
MostafaRhazlani <mo.rhazlani@gmail.com>
2023-12-25 00:08:01 +0100

    add role user and admin

---
+++ app/Enums/RoleType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum RoleType: int
+{
+    case admin = 1;
+    case user = 2;
+}
+
+?>
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\RoleType;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -42,4 +44,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function role(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => RoleType::from($value)->name,
+        );
+    }
 }
